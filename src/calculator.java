415307457937ce34bd671f4c82ef767e1d0250d7
Barkenov Dimash <dimashbarkenov@gmail.com>
2023-01-16 13:46:14 +0600

    Initial commit

---
+++ src/calculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        double num1;
+        double num2;
+        double ans;
+        double op;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter two numbers:");
+        num1 = reader.nextDouble();
+        num2 = reader.nextDouble();
+        System.out.print("\nEnter an operator (+,-,*,/) : ");
+        op = reader.next().charAt(0);
+        switch ((int) op){
+            case '+': ans = num1 + num2;
+            break;
+            case '-': ans = num1 - num2;
+            break;
+            case '*': ans = num1 * num2;
+            break;
+            case '/': ans = num1 / num2;
+            break;
+            default:
+                System.out.printf("Error! Enter correct operator");
+                return;
+        }
+        System.out.print("\nThe result is giben as sollows:\n");
+        System.out.printf(num1 + " " + op + " " + num2 + "=" + ans);
+    }
+}
